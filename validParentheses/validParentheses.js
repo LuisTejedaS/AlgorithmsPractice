24a5e51cd283397f71c4bff02fbfa2dc557effd6
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2023-12-18 16:10:28 -0600

    formatting

--- validParentheses/validParentheses.js
+++ validParentheses/validParentheses.js
@@ -1,18 +1,19 @@
-function perform(value){
+function perform(value) {
     let stack = []
-    let closeToOpen = { ")": "(", "}": "{", "]": "["}
+    let closeToOpen = { ")": "(", "}": "{", "]": "[" }
 
-    for (let index = 0; index < value.length; index++){
+    for (let index = 0; index < value.length; index++) {
         let c = value[index];
-        if(closeToOpen[c]){
-            if(stack.length > 0 && stack[stack.length-1] === closeToOpen[c]){
+        if (closeToOpen[c]) {
+            if (stack.length > 0 && stack[stack.length - 1] === closeToOpen[c]) {
+                // if (stack.length > 0 && stack.at(-1) === closeToOpen[c]) { //since node 16.6
                 stack.pop()
             }
             else {
                 return false;
             }
         }
-        else if(Object.values(closeToOpen).find(x=> { return c === x})){
+        else if (Object.values(closeToOpen).find(x => { return c === x })) {
             stack.push(c)
         }
 
