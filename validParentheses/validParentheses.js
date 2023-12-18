0ec6371f9e42cb132bd64cc5a7a06c8f4d806b03
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2023-12-18 13:22:15 -0600

    add parentheses

---
+++ validParentheses/validParentheses.js
@@ -0,0 +1,26 @@
+function perform(value){
+    let stack = []
+    let closeToOpen = { ")": "(", "}": "{", "]": "["}
+
+    for (let index = 0; index < value.length; index++){
+        let c = value[index];
+        if(closeToOpen[c]){
+            if(stack.length > 0 && stack[stack.length-1] === closeToOpen[c]){
+                stack.pop()
+            }
+            else {
+                return false;
+            }
+        }
+        else if(Object.values(closeToOpen).find(x=> { return c === x})){
+            stack.push(c)
+        }
+
+    }
+    return stack.length === 0;
+}
+
+console.log(perform("(s)"))
+console.log(perform(")("))
+console.log(perform("[)(]"))
+console.log(perform("(){}[]"))
